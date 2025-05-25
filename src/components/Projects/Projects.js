d711a74d7417abd41cef139bcddff21b6575d36c
Kumud Raj Ghimire <kumudraj.cs23@bmsce.ac.in>
2025-05-25 10:52:36 +0530

    updated projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,6 +6,8 @@ import Particle from "../Particle";
 import bus from "../../Assets/Projects/bustracking.png";
 import dormly from "../../Assets/Projects/ecommerce.png";
 import app from "../../Assets/Projects/dormly_app.png";
+import skill from "../../Assets/Projects/skill.png";
+import note from "../../Assets/Projects/exnote.png";
 
 function Projects() {
   return (
@@ -32,12 +34,38 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
+              imgPath={skill}
+              isBlog={false}
+              title="Skill Trade App"
+              
+              description="SkillTrade, a Flutter-powered mobile platform, facilitates item and skill exchange within communities. Powered by Google Firebase (FirebaseAuth for secure authentication, Cloud Firestore for scalable data), it moves beyond traditional monetary transactions. This innovative app connects individuals, unlocking underutilized assets and fostering a collaborative sharing ecosystem. It is responsive, reliable, and future-proof application."
+              ghLink="https://github.com/KumudRajGhimire/skill-trade.git"
+              
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={note}
+              isBlog={false}
+              title="Exnote Web App"
+              description={
+                <span>
+                  This free, **TypeScript**-powered note-sharing website for **BMSCE** students offers a vast repository of **PYQ (Previous Year Questions)**. With over **3500 active users**, it's a dynamic platform designed to enhance academic collaboration and resource accessibility for BMS College of Engineering. Check out the live application at <a href="https://exnote.vercel.app" target="_blank" rel="noopener noreferrer">exnote.vercel.app</a>.
+                </span>
+              }
+              ghLink="https://github.com/KumudRajGhimire/exnote.git"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
               imgPath={app}
               isBlog={false}
               title="Dormly app"
               
               description="Dormly Application – A cross-platform Flutter-based app currently under development. It utilizes Supabase for backend services and Flask for additional server-side functionality. The UI/UX is designed using Figma, ensuring a seamless and intuitive user experience across all devices. The app aims to provide a smooth and efficient solution for dorm management, enhancing convenience for users."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
+              ghLink="https://github.com/GhanshyamSharma122/dormly.git"
               
             />
           </Col>
